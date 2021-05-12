fea25055c485738d298df4b134c9498ccaec236a
Ghita Alexandru <ghita.alexandru6@gmail.com>
2021-05-12 15:30:12 +0100

    complete views and template for the other operations

--- website/views.py
+++ website/views.py
@@ -32,12 +32,75 @@ def add(request):
 
 
 def subtract(request):
-    return render(request, 'subtract.html', {})
+    num1 = randint(0, 10)
+    num2 = randint(0, 10)
+    if request.method == 'POST':
+        answer = request.POST['answer']
+        old_num1 = request.POST['old_num1']
+        old_num2 = request.POST['old_num2']
+        correct_answer = int(old_num1) - int(old_num2)
+        if int(answer) == correct_answer:
+            my_answer = 'Correct ' + old_num1 + '-' + old_num2 + '=' + answer
+            color = 'success'
+        else:
+            my_answer = 'Incorrect ' + old_num1 + '-' + old_num2 + ' is not ' + answer + ' it is ' + str(correct_answer)
+            color = 'danger'
+        return render(request, 'subtract.html', {'answer': answer,
+                                                 'my_answer': my_answer,
+                                                 'num1': num1,
+                                                 'num2': num2,
+                                                 'color': color})
+    return render(request, 'subtract.html', {
+        'num1': num1,
+        'num2': num2
+    })
 
 
 def multiply(request):
-    return render(request, 'multiply.html', {})
+    num1 = randint(0, 10)
+    num2 = randint(0, 10)
+    if request.method == 'POST':
+        answer = request.POST['answer']
+        old_num1 = request.POST['old_num1']
+        old_num2 = request.POST['old_num2']
+        correct_answer = int(old_num1) * int(old_num2)
+        if int(answer) == correct_answer:
+            my_answer = 'Correct ' + old_num1 + '*' + old_num2 + '=' + answer
+            color = 'success'
+        else:
+            my_answer = 'Incorrect ' + old_num1 + '*' + old_num2 + ' is not ' + answer + ' it is ' + str(correct_answer)
+            color = 'danger'
+        return render(request, 'multiply.html', {'answer': answer,
+                                                 'my_answer': my_answer,
+                                                 'num1': num1,
+                                                 'num2': num2,
+                                                 'color': color})
+    return render(request, 'multiply.html', {
+        'num1': num1,
+        'num2': num2
+    })
 
 
 def divide(request):
-    return render(request, 'divide.html', {})
+    num1 = randint(0, 10)
+    num2 = randint(0, 10)
+    if request.method == 'POST':
+        answer = request.POST['answer']
+        old_num1 = request.POST['old_num1']
+        old_num2 = request.POST['old_num2']
+        correct_answer = int(old_num1) / int(old_num2)
+        if int(answer) == correct_answer:
+            my_answer = 'Correct ' + old_num1 + '/' + old_num2 + '=' + answer
+            color = 'success'
+        else:
+            my_answer = 'Incorrect ' + old_num1 + '/' + old_num2 + ' is not ' + answer + ' it is ' + str(correct_answer)
+            color = 'danger'
+        return render(request, 'divide.html', {'answer': answer,
+                                               'my_answer': my_answer,
+                                               'num1': num1,
+                                               'num2': num2,
+                                               'color': color})
+    return render(request, 'divide.html', {
+        'num1': num1,
+        'num2': num2
+    })
