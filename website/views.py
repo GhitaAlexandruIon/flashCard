5ec3834694c67517099b61b40253c41c4268ea03
Ghita Alexandru <ghita.alexandru6@gmail.com>
2021-05-12 15:55:47 +0100

    float answer for divide operation

--- website/views.py
+++ website/views.py
@@ -83,13 +83,13 @@ def multiply(request):
 
 def divide(request):
     num1 = randint(0, 10)
-    num2 = randint(0, 10)
+    num2 = randint(1, 10)
     if request.method == 'POST':
         answer = request.POST['answer']
         old_num1 = request.POST['old_num1']
         old_num2 = request.POST['old_num2']
         correct_answer = int(old_num1) / int(old_num2)
-        if int(answer) == correct_answer:
+        if float(answer) == correct_answer:
             my_answer = 'Correct ' + old_num1 + '/' + old_num2 + '=' + answer
             color = 'success'
         else:
