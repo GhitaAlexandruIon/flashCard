6e70db542f2b879674b3b6cb9bd7f59e728c4058
Ghita Alexandru <ghita.alexandru6@gmail.com>
2021-05-12 15:16:57 +0100

    added images for the calculation

--- website/views.py
+++ website/views.py
@@ -16,9 +16,15 @@ def add(request):
         correct_answer = int(old_num1) + int(old_num2)
         if int(answer) == correct_answer:
             my_answer = 'Correct ' + old_num1 + '+' + old_num2 + '=' + answer
+            color = 'success'
         else:
             my_answer = 'Incorrect ' + old_num1 + '+' + old_num2 + ' is not ' + answer + ' it is ' + str(correct_answer)
-        return render(request, 'add.html', {'answer': answer, 'my_answer': my_answer, 'num1': num1, 'num2': num2})
+            color = 'danger'
+        return render(request, 'add.html', {'answer': answer,
+                                            'my_answer': my_answer,
+                                            'num1': num1,
+                                            'num2': num2,
+                                            'color': color})
     return render(request, 'add.html', {
         'num1': num1,
         'num2': num2
